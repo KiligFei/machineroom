56da7995a5206db9386c065b61821aab0e14119c
Kilig <1775814844@qq.com>
2022-06-13 09:55:41 +0800

    feat: :sparkles: 机柜Models鼠标移入事件

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import './App.css'
+import MachineRoom from './component/MachineRoom'
 
-function App() {
-  return (
-    <div className="App">
-      <canvas id="canvas"></canvas>
-    </div>
-  )
+// 机房对象
+let room: MachineRoom
+// canvas画布
+let canvas: HTMLCanvasElement
+
+class App extends React.Component {
+  // 组件挂载完成后，实例化机房对象，渲染机房
+  componentDidMount(): void {
+    if (!canvas)
+      return null
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    room = new MachineRoom(canvas)
+    room.loadGLTF('machineRoom.gltf')
+    room.animate()
+  }
+
+  // 鼠标移入事件
+  mouseMove({ clientX, clientY }) {
+    room.selectCabinet(clientX, clientY)
+  }
+
+  render() {
+    return (
+      <div className="App" onMouseMove={this.mouseMove}>
+        <canvas id="canvas" ref={ele => canvas = ele}></canvas>
+      </div>
+    )
+  }
 }
 
 export default App
---
+++ src/component/MachineRoom.ts
@@ -0,0 +1,145 @@
+import type {
+  Color,
+  Mesh,
+  MeshStandardMaterial,
+  Texture,
+} from 'three'
+import {
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Raycaster,
+  Scene,
+  TextureLoader,
+  Vector2,
+  WebGLRenderer,
+} from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+// GLTF 加载器
+const gltfLoader = new GLTFLoader()
+// 射线投射器
+const raycaster = new Raycaster()
+// 鼠标在裁剪空间中的点位
+const pointer = new Vector2()
+export default class MachineRoom {
+  // 渲染器
+  renderer: WebGLRenderer
+  // 场景
+  scene: Scene
+  // 相机
+  camera: PerspectiveCamera
+  // 轨道控制器
+  controls: OrbitControls
+  // 存放模型文件的目录
+  modelPath: string
+  // 纹理集合
+  maps: Map<string, Texture> = new Map()
+  // 机柜集合
+  cabinets: Mesh[] = []
+  // 鼠标滑入的机柜
+  curCabinet: Mesh
+  // 鼠标划入机柜事件，参数为机柜对象
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  onMouseOverCabinet = (cabinet: Mesh) => { }
+  // 鼠标在机柜上移动的事件，参数为鼠标在canvas画布上的坐标位
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  onMouseMoveCabinet = (x: number, y: number) => { }
+  // 鼠标划出机柜的事件
+  onMouseOutCabinet = () => { }
+
+  // 初始化场景
+  constructor(canvas: HTMLCanvasElement, modelPath = './models/') {
+    this.renderer = new WebGLRenderer({ canvas })
+    this.scene = new Scene()
+    this.camera = new PerspectiveCamera(45, canvas.width / canvas.height, 0.1, 1000)
+    this.camera.position.set(0, 10, 15)
+    this.camera.lookAt(0, 0, 0)
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+    this.modelPath = modelPath
+    this.crtTexture('cabinet-hover.jpg')
+  }
+
+  // 加载GLTF模型
+  loadGLTF(modelName = '') {
+    gltfLoader.load(this.modelPath + modelName, ({ scene: { children } }) => {
+      children.forEach((obj: Mesh) => {
+        const { map, color } = obj.material as MeshStandardMaterial
+        this.changeMat(obj, map, color)
+        if (obj.name.includes('cabinet'))
+          this.cabinets.push(obj)
+      })
+      this.scene.add(...children)
+    })
+  }
+
+  // 修改材质
+  changeMat(obj: Mesh, map: Texture, color: Color) {
+    if (map) {
+      obj.material = new MeshBasicMaterial({
+        map: this.crtTexture(map.name),
+      })
+    }
+    else {
+      obj.material = new MeshBasicMaterial({ color })
+    }
+  }
+
+  // 建立纹理对像
+  crtTexture(imgName: string) {
+    let curTexture = this.maps.get(imgName)
+    if (!curTexture) {
+      curTexture = new TextureLoader().load(this.modelPath + imgName)
+      curTexture.flipY = false
+      curTexture.wrapS = 1000
+      curTexture.wrapT = 1000
+      this.maps.set(
+        imgName,
+        curTexture,
+      )
+    }
+    return curTexture
+  }
+
+  // 选择机柜
+  selectCabinet(x: number, y: number) {
+    const { cabinets, renderer, camera, maps, curCabinet } = this
+    const { width, height } = renderer.domElement
+    // 鼠标的canvas坐标转裁剪坐标
+    pointer.set((x / width) * 2 - 1, -(y / height) * 2 + 1)
+    // 基于鼠标点的裁剪坐标位和相机设置射线投射器
+    raycaster.setFromCamera(pointer, camera)
+    // 选择柜机
+    const intersect = raycaster.intersectObjects(cabinets)[0]
+    const intersectObj = intersect ? intersect.object as Mesh : null
+    // 若之前被选
+    if (curCabinet && curCabinet !== intersectObj) {
+      const material = curCabinet.material as MeshBasicMaterial
+      material.setValues({
+        map: maps.get('cabinet.jpg'),
+      })
+    }
+    if (intersectObj) {
+      this.onMouseMoveCabinet(x, y)
+      if (intersectObj !== curCabinet) {
+        this.curCabinet = intersectObj
+        const material = intersectObj.material as MeshBasicMaterial
+        material.setValues({
+          map: maps.get('cabinet-hover.jpg'),
+        })
+        this.onMouseOverCabinet(intersectObj)
+      }
+    }
+    else if (curCabinet) {
+      this.curCabinet = null
+      this.onMouseOutCabinet()
+    }
+  }
+
+  // 连续渲染
+  animate() {
+    this.renderer.render(this.scene, this.camera)
+    requestAnimationFrame(() => {
+      this.animate()
+    })
+  }
+}
